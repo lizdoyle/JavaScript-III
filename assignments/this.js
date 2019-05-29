9f3302bc17b0bd38b1f5084422d24c6bf8936f8d
lizdoyle <lizdoyle1121@gmail.com>
2019-05-28 21:39:14 -0400

    started on this.js

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,11 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* The Four Principles of the "this" keyword are as follows:
+* 1. Global/Window Binding - (in the browser / Window Binding)keeps the "this" keyword attached to the entire Window object of Javascript. 'This" is not actually pointing to one specific thing, and therefore points to everything (all of javascript inside of the Window object). Global Binding points to the actual nodejs. 
+* 2. Implicit Binding - occurs when the "this" keyword is attached to a object. The object is is attached to is referenced before the dot (.). For example. "name.this" points to the function "name".
+* 3. New Binding
+* 4. Explicit Binding
 *
 * write out a code example of each explanation above
 */
@@ -19,6 +20,8 @@
 // Principle 2
 
 // code example for Implicit Binding
+
+
 
 // Principle 3
 
